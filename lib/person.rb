46fee38ca4200f47676e74988db7ff64483cbcc9
trose618 <trose618@gmail.com>
2018-11-02 12:38:27 -0400

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,4 +1,3 @@
-require 'pry'
 class Person
   #your code here
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
@@ -7,7 +6,6 @@ class Person
 
     attributes.each do |key, value|
       self.send(("#{key}="), value)
-      binding.pry
     end
   end
 
